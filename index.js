ac79531c37b49e48cc19820f9bba174243d540b4
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-03-09 21:08:52 +0530

    countWords

--- index.js
+++ index.js
@@ -30,12 +30,31 @@ function countWords(paragraph, firstWord, secondWord) {
  * The function takes raw text as a parameter and returns the clean text.
  */
 
-const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. 
+let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. 
     There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;
     I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate
     yo@u to be a tea@cher!?`;
-console.log(cleanText(sentence));
+// console.log(cleanText(sentence));
 
 function cleanText(sentence) {
   return sentence.replace(/[|&;$%@"<>()+,]/g, "");
 }
+
+//  ########################################################################
+
+/**
+ * 1.c. After cleaning the text in the sentence from question number b you will get
+ * the following text.
+ * Count the number of words in this text. Don't include words with only one letter.
+ */
+
+sentence = cleanText(sentence);
+console.log(countWords(sentence));
+
+function countWords(sentence) {
+  const totalWords = sentence.split(" ").filter((v) => v.length > 1).length;
+  return totalWords;
+}
+
+// ################################################################################
+
